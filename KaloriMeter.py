98674f5fdea5092bb022414990a543135a6b9d00
Rachmat Hidayat <rachmathidayat@pallakastudio.com>
2022-09-04 23:36:36 +0800

    new env

--- KaloriMeter.py
+++ KaloriMeter.py
@@ -4,26 +4,70 @@ import matplotlib.pyplot as plt
 import skimage.io
 import skimage.color
 import skimage.filters
-from skimage import data, filters, measure, morphology
+from skimage.draw import polygon_perimeter
+from skimage.measure import label, regionprops, perimeter
+from skimage import filters, measure, morphology
 import cv2 as cv
 
 class KaloriMeter:
-    def segment(self, image_path):
-        image = cv.imread(image_path, 0)
+    def segment(self, height_image_path, area_image_path):
+        # calculate height of object
+        height_image = cv.imread(height_image_path, 0)
+        blurred_height_image = cv.GaussianBlur(height_image, (5,5), 0)
+        th = filters.threshold_otsu(blurred_height_image)
+        binary_height_mask = blurred_height_image > th
+        binary_height_mask = morphology.remove_small_objects(binary_height_mask, 50)
+        binary_height_mask = morphology.remove_small_holes(binary_height_mask, 50)
 
-        blurred_image = cv.GaussianBlur(image, (5,5), 0)
-        t = filters.threshold_otsu(blurred_image)
-        binary_mask = blurred_image > t
-        binary_mask = morphology.remove_small_objects(binary_mask, 50)
-        binary_mask = morphology.remove_small_holes(binary_mask, 50)
-        labels = measure.label(binary_mask)
-        for region in measure.regionprops(labels, image):
+        # create bounding box
+        area_image_labels = label(binary_height_mask)
+        bounding_box = []
+        for region in regionprops(np.squeeze(area_image_labels), height_image):
+            bbox = region.bbox
+            print(region)
+            bounding_box.append(bbox)
+        print(bounding_box)
+
+        with_boxes = np.copy(binary_height_mask)
+
+        height = []
+        for box in bounding_box:
+            #[Xmin, Xmax, Ymin, Ymax]
+            r = [box[2],box[0],box[0],box[2]]
+            c = [box[3],box[3],box[1],box[1]]
+            rr, cc = polygon_perimeter(r, c, binary_height_mask.shape)
+            with_boxes[rr, cc] = 1 #set color white
+            
+            # calculate height
+            rmax = box[2]
+            rmin = box[0]
+            height.append([(rmax-rmin) * 0.026458333])
+
+
+
+
+        # calculate area of object
+        area_image = cv.imread(area_image_path, 0)
+        blurred_area_image = cv.GaussianBlur(area_image, (5,5), 0)
+        ta = filters.threshold_otsu(blurred_area_image)
+        binary_area_mask = blurred_area_image > ta
+        binary_area_mask = morphology.remove_small_objects(binary_area_mask, 50)
+        binary_area_mask = morphology.remove_small_holes(binary_area_mask, 50)
+
+        height_image_labels = label(binary_area_mask)
+            
+        areas = []
+        for region in regionprops(height_image_labels, area_image):
             area = region.area
-            area = area / 78
-            print(area)
+            area = area * 0.026458333
+            # print(area)
 
-
-        return labels.reshape((image.shape[0],image.shape[1])),binary_mask.astype(np.uint8)
+        # calculate perimeter of object
+        # perimeter = perimeter(binary_mask, 4) * 0.026458333
+        
+        height = np.max(height)
+        volume = area * height
+        return area, height, volume
 
     def calories(self, binary_mask):
         w = binary_mask.shape[1]
--- api.py
+++ api.py
@@ -1,20 +1,30 @@
-from pydoc import resolve
-import pandas as pd
-import numpy as np
-import pickle
-from PIL import Image
-from base64 import encodebytes
-import cv2
+# TO DO LIST
+# - Selesaikan api logic
+# - Tambahkan variabel2 kondisional seperti jenis makanan, dan konversi bytes to cm
+# note: bisa juga perhitungan kalori di sisi front end, jadi di back end hanya menghitung luas dari objek
+
+
+
+
+# from email.mime import image
+# from operator import ne
+# from pydoc import resolve
+# import pandas as pd
+# import numpy as np
+# import pickle
+# import cv2
+# import argparse
+# import sys
+# from keras.models import load_model
+# from tensorflow.keras.preprocessing.image import load_img, img_to_array
+# from flask_restful import Resource, Api
+# from PIL import Image
+# from segment import Segment
+# from base64 import encodebytes
 import os
 from KaloriMeter import KaloriMeter
-from segment import Segment
-import argparse
-import sys
 from flask_cors import cross_origin, CORS
-from keras.models import load_model
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
-from flask import Flask, request, jsonify, make_response
-from flask_restful import Resource, Api
+from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
@@ -41,27 +51,38 @@ def index():
         response.headers.add("Access-Control-Allow-Origin", "*")
         return response
     elif request.method == "POST":
-        if 'images[]' not in request.files:
+        if 'height_image' and 'area_image' not in request.files:
             resp = jsonify({'message' : 'No file part in the request'})
             resp.status_code = 400
             return resp
             
-        images = request.files.getlist('images[]')
+        height_image = request.files['height_image']
+        area_image = request.files['area_image']
+
         errors = {}
         success = False
+
+        if height_image and allowed_file(height_image.filename):
+            filename = secure_filename(height_image.filename)
+            height_image.save(os.path.join(app.config['UPLOAD_FOLDER'] + 'height/', filename))
+            height_image_path=os.path.join(app.config['UPLOAD_FOLDER'] + 'height/', filename)
+            success = True
+        else:
+            errors[height_image.filename] = 'File type is not allowed'
+
+        if area_image and allowed_file(area_image.filename):
+            filename = secure_filename(area_image.filename)
+            area_image.save(os.path.join(app.config['UPLOAD_FOLDER'] + 'area/', filename))
+            area_image_path=os.path.join(app.config['UPLOAD_FOLDER'] + 'area/', filename)
+            success = True
+        else:
+            errors[area_image.filename] = 'File type is not allowed'
         
-        image_file = {}
-        for image in images:
-            if image and allowed_file(image.filename):
-                filename = secure_filename(image.filename)
-                image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                success = True
-                image_file[image.filename] = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            else:
-                errors[image.filename] = 'File type is not allowed'
+        kalorimeter = KaloriMeter()
 
-        # image processing
+        calculation = kalorimeter.segment(height_image_path, area_image_path)
 
+        
         
 
 
@@ -131,6 +152,21 @@ if __name__ == "__main__":
 #             response = make_response(buffer.tobytes())
 #             response.headers['Content-Type'] = 'image/png'
 #         return jsonify(segmented_images)
+
+
+# updated multiple image
+# image_file = {}
+        # for image in images:
+        #     if image and allowed_file(image.filename):
+        #         filename = secure_filename(image.filename)
+        #         image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #         success = True
+        #         image_file[image.filename] = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        #     else:
+        #         errors[image.filename] = 'File type is not allowed'
+
+        # image processing
+        # segment = KaloriMeter()
 
 
 
